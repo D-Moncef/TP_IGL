d4e7add184e51e729f341f0912dcdbddb70af29e
D-Moncef <djeddoumoncef@gmail.com>
2026-02-01 21:56:14 +0100

    Update file_writer.py

--- src/tools/file_writer.py
+++ src/tools/file_writer.py
@@ -1,27 +1,27 @@
 import os 
 
 from pathlib import Path
-def write_file(path_to_file :str,file_name:str, content:str):
-       "write a  file  in the send box  the  file  must  located  in sand box and the  file  must  be  python"
-       fullpath=os.path.join(path_to_file,file_name)
-        
+def write_file(path_to_file :str, content:str):
+       "write a  file  in the sandbox  the  file  must  located  in sand box and the  file  must  be  python"
+       
        if  not os.path.exists(path_to_file):
            raise Exception(f"path {path_to_file} does not exist")
-       if "sendbox" not  in fullpath :
+       if "sandbox" not  in path_to_file :
                raise Exception("path to file  not valid")
-       if  not file_name.endswith('.py'): 
-           raise Exception("file {file_name} format not  valid ")
+       if  not path_to_file.endswith('.py'): 
+           raise Exception(f"file {Path(path_to_file).name} format not  valid ")
        
        try:
-        with open(fullpath, 'w', encoding='utf-8') as file:
+           with open(path_to_file, 'w', encoding='utf-8') as file:
            file.write(content)
+       except PermissionError as e
+           raise PermissionError(f"Failed to write file {fullpath}: {e}")
        except Exception as e:
-         print(f"Failed to write file {fullpath}: {e}")
-         return False
+           raise Exception(f"Failed to write file {fullpath}: {e}")
+           return False
 
        return True
-content="# yo)\n"
-write_file("/home/yacineyo/refactoring-swarm-template/sendbox","c.py",content)
+
      
        
      
