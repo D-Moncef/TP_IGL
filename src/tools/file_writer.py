a4f347a10792c32d6c776f44431ec82cda16748d
D-Moncef <djeddoumoncef@gmail.com>
2026-02-02 00:09:39 +0100

    Update file_writer.py to be able to add test files

--- src/tools/file_writer.py
+++ src/tools/file_writer.py
@@ -1,15 +1,21 @@
 import os 
 
 from pathlib import Path
-def write_file(path_to_file :str, content:str):
-       "write a  file  in the sandbox  the  file  must  located  in sand box and the  file  must  be  python"
-       
-       if  not os.path.exists(path_to_file):
-           raise Exception(f"path {path_to_file} does not exist")
-       if "sandbox" not  in path_to_file :
-               raise Exception("path to file  not valid")
-       if  not path_to_file.endswith('.py'): 
-           raise Exception(f"file {Path(path_to_file).name} format not  valid ")
+def write_file(path_to_file :str, content:str, test : bool):
+    "write a  file  in the sandbox  the  file  must  located  in sand box and the  file  must  be  python"
+
+        if "sandbox" not  in path_to_file :
+            raise Exception("path to file  not valid")
+        if not path_to_file.endswith('.py'): 
+            raise Exception(f"file {Path(path_to_file).name} format not  valid ")
+              
+        if not test:
+           if  not os.path.exists(path_to_file):
+               raise Exception(f"path {path_to_file} does not exist")
+        else
+           if not (Path(path_to_file).name.startswith("test_") or Path(path_to_file).name.endswith("_test.py")):
+               raise Exception(f"path {path_to_file} is not a valid test file path")
+           Path(path_to_file).parent.mkdir(parents=True, exist_ok=True)
        
        try:
            with open(path_to_file, 'w', encoding='utf-8') as file:
@@ -17,7 +23,7 @@ def write_file(path_to_file :str, content:str):
        except PermissionError as e
            raise PermissionError(f"No permission to write file {fullpath}: {e}")
        except Exception as e:
-           raise Exception(f"Failed to write file {fullpath}: {e}")
+           raise Exception(f"Failed to write file {path_to_file}: {e}")
            return False
 
        return True
